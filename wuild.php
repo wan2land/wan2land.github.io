ee5b28caf92048d0d88daf6cc2da5d35d858e5ec
Changwan Jun <me@wani.kr>
2015-04-13 16:31:47 +0900

    wuild task apply version 1.0-dev

--- wuild.php
+++ wuild.php
@@ -1,17 +1,15 @@
 <?php
 use Wuild\Console;
 
-Wuild::task(
-    "create.post",
-    function() {
-        // $this->arguments 사용가능해야함.
-        if (!isset($_SERVER['argv'][2])) {
-            return false;
-        }
-        $subject = $_SERVER['argv'][2];
-        $fileName = '_posts/' . date('Y-m-d') . '-' . $subject . '.md';
-        $date = date('Y-m-d H:i:s');
-        file_put_contents($fileName, "---
+Wuild::task('create:post', function() {
+    // $this->arguments 사용가능해야함.
+    if (!isset($_SERVER['argv'][2])) {
+        return false;
+    }
+    $subject = $_SERVER['argv'][2];
+    $fileName = '_posts/' . date('Y-m-d') . '-' . $subject . '.md';
+    $date = date('Y-m-d H:i:s');
+    file_put_contents($fileName, "---
 layout: post
 title: \"{$subject}\"
 date: {$date}
@@ -19,7 +17,6 @@ categories:
 ---
 
 ");
-        Console::exec("open {$fileName}");
-        Console::writeLine("Create Success!");
-    }
-);
+    Console::execute("open {$fileName}");
+    Console::writeLine("Create Success!");
+});
