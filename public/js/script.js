1e59fd6e790df44b6223430b3b5a847c15eccc7c
Changwan Jun <wan2land@gmail.com>
2023-01-03 10:12:16 +0900

    chore: update GA

--- public/js/script.js
+++ public/js/script.js
@@ -1,12 +1,3 @@
-// GA
-(function(i,s,o,g,r,a,m){i["GoogleAnalyticsObject"]=r;i[r]=i[r]||function(){
-(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,"script","//www.google-analytics.com/analytics.js","ga")
-
-ga("create", "UA-35736735-1", "wan2.land")
-ga("send", "pageview")
-
 // anchor.js
 anchors.add(".post-content h2, .post-content h3")
 
